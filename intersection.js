3c1ec41db39c2f37dfbde0e229c35b7d1ae3a98f
Skyler Escobedo <skylerescobedo@gmail.com>
2017-12-18 19:04:07 -0800

    Solution Intersection.js

--- intersection.js
+++ intersection.js
@@ -1,17 +1,7 @@
-// Construct a function union that compares input arrays and returns a new array that contains all elements. 
-// If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting 
-// from the first element of the first input array. BONUS: Use reduce!
-function union(...arrays) {
-    let output = [...arrays[0], ...arrays[1], ...arrays[2]]
-    
-    let result = [];
-    for (let i = 0; i < output.length; i+=1) {
-        if(result.includes(output[i]) === false){
-            result.push(output[i]);
-        }
-    }
-    return result;
+// Construct a function intersection that compares input arrays and returns a new array with elements found in all 
+// of the inputs. BONUS: Use reduce!
+function intersection(...arrays) {
+    return arrays.reduce((acc, val, arr) => acc.filter(ele => val.includes(ele)));
 }
-
-console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
-// should log: [5, 10, 15, 88, 1, 7, 100]
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+// should log: [5, 15]
